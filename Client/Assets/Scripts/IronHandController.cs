f789da45e7b89815365aabaa4945864f9af0e047
咪咪 <>
2021-11-28 15:27:18 +0800

    no message

--- Client/Assets/Scripts/IronHandController.cs
+++ Client/Assets/Scripts/IronHandController.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class IronHandController : MonoBehaviour
 {
+
+    Vector3 point;
+    GameObject effectGo;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        effectGo = Resources.Load<GameObject>("Prefabs/hanjieParticle");
     }
 
     // Update is called once per frame
@@ -15,5 +20,16 @@ public class IronHandController : MonoBehaviour
     {
         transform.position = Input.mousePosition;
         transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, 0);
+
+        if(Input.GetMouseButton(0))
+        {
+
+            point = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 4f);//获得鼠标点击点
+            point = Camera.main.ScreenToWorldPoint(point);//从屏幕空间转换到世界空间
+            GameObject go = Instantiate(effectGo);//生成特效
+            go.transform.position = point;
+            Destroy(go, 0.5f);
+            Debug.Log("clicl");
+        }
     }
 }
