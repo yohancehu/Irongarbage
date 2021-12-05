9e2d771416917f3c81804331afa8baf7c01d203d
咪咪 <>
2021-12-05 16:18:37 +0800

    no message

---
+++ Client/Assets/Scripts/LoadClose.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadClose : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadGames() {
+        SceneManager.LoadScene(1);
+
+    }
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Client/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadGames() {
+        SceneManager.LoadScene(0);
+
+    }
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
