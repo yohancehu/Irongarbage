c9910372324d39c7b29d00fb0c4c193acc0186b8
咪咪 <>
2022-01-29 21:56:47 +0800

    no message

--- Client/Assets/bl旺财/Assets/Scripts/LoseUI.cs
+++ Client/Assets/bl旺财/Assets/Scripts/LoseUI.cs
@@ -1,10 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoseUI : MonoBehaviour
 {
+    public Text context;
+
+    public void SetText(string content)
+    {
+        context.text = content;
+    }
     public void Restart()
     {
         SceneManager.LoadScene(0);
---
+++ Client/Assets/bl旺财/Assets/Scripts/Spike.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if(other.CompareTag("Player"))
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/Lose");
+            GameObject obj = Instantiate(prefab, UIRoot.Instance.transform);
+            LoseUI ui = obj.GetComponent<LoseUI>();
+            ui.SetText("你被扎死了。");
+        }
+    }
+}
